66b7d3b4b96ca19376fd437a9d70d5b4ea0ccbf9
kralonur <kralonur1998@gmail.com>
2022-07-05 17:12:56 +0700

    test: update eip712-draft test

--- test/eip712demodraft/EIP712DemoDraft.ts
+++ test/eip712demodraft/EIP712DemoDraft.ts
@@ -90,6 +90,61 @@ describe("EIP712DemoDraft tests", function () {
         ),
       ).equal(this.signers.admin.address);
     });
+
+    it("Should return signature owner from contract(v2)", async function () {
+      const name = "Struct";
+      const version = "1";
+      const chainId = Number(await network.provider.send("eth_chainId"));
+      const verifyingContract = this.eip712DemoDraft.address;
+
+      const from = this.signers.admin.address;
+      const targetContract = this.eip712DemoDraft.address;
+      const chainTo = Number(await network.provider.send("eth_chainId"));
+      const nonce = await this.signers.admin.getTransactionCount();
+
+      const typedData = {
+        domain: {
+          name: name,
+          version: version,
+          chainId: chainId,
+          verifyingContract: verifyingContract,
+        },
+        types: {
+          Struct: [
+            { name: "from", type: "address" },
+            { name: "targetContract", type: "address" },
+            { name: "chainTo", type: "uint256" },
+            { name: "nonce", type: "uint256" },
+          ],
+        },
+        value: {
+          from: from,
+          targetContract: targetContract,
+          chainTo: chainTo,
+          nonce: nonce,
+        },
+      };
+
+      const signedMessage = await this.signers.admin._signTypedData(typedData.domain, typedData.types, typedData.value);
+      console.log(`signedMessage: `, signedMessage);
+
+      const signature = ethers.utils.splitSignature(signedMessage);
+      console.log(`signature: `, signature);
+
+      console.log(ethers.utils.verifyTypedData(typedData.domain, typedData.types, typedData.value, signedMessage));
+
+      expect(
+        await this.eip712DemoDraft.getAddress(
+          from,
+          targetContract,
+          chainTo,
+          nonce,
+          signature.v,
+          signature.r,
+          signature.s,
+        ),
+      ).equal(this.signers.admin.address);
+    });
   });
 });
 
